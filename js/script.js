c6fa4a66cb420559641928df864a7f27fb1a1e47
F-Sampaio <f.s1488@outlook.com>
2019-07-23 21:21:29 -0400

    ajustes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ citySearch.addEventListener('click', search);
     var cityName = document.getElementById('cityName').innerHTML;
 
     var http = new XMLHttpRequest();
-    var url = `ahttp://api.openweathermap.org/data/2.5/find?q=${cityName}&units=metric&type=accurate&mode=xml&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e`;
+    var url = `http://api.openweathermap.org/data/2.5/find?q=${cityName}&units=metric&type=accurate&mode=xml&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e`;
 
     http.open("GET", url, true);
     http.send();
