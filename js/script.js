793a48b1b81cb5e55bd7369ca8bf336bb31b4e93
F-Sampaio <f.s1488@outlook.com>
2019-07-23 21:44:30 -0400

    ajustes na requisição http, retorno correto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 citySearch.addEventListener('click', search);
 
  function search(){
-    var cityName = document.getElementById('cityName').innerHTML;
+    var cityName = document.getElementById('cityName').value;
 
     var http = new XMLHttpRequest();
-    var url = `http://api.openweathermap.org/data/2.5/find?q=${cityName}&units=metric&type=accurate&mode=xml&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e`;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e`;
 
     http.open("GET", url, true);
     http.send();
 
     http.onreadystatechange = function() {
-        console.log(cityName);
         console.log(http.responseText)
     }
 
