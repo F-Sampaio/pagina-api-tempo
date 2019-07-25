49f8c834b6858f001741e2c814b8009652e60448
F-Sampaio <f.s1488@outlook.com>
2019-07-24 20:41:12 -0400

    tratamento json

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ citySearch.addEventListener('click', search);
     var cityName = document.getElementById('cityName').value;
 
     var http = new XMLHttpRequest();
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e`;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=b3a40c8e9ed10a3850cb4e90c6a5305e&units=metric`;
 
     http.open("GET", url, true);
     http.send();
 
     http.onreadystatechange = function() {
-        console.log(http.responseText)
+        var weather = JSON.parse(http.responseText)
+        // console.log(weather);
+        console.log(`Cidade: ${weather.name}`)
+        console.log(`Atual: ${Math.round(parseFloat(weather.main.temp))}`);
+
+        // console.log(http.responseText)
     }
 
 }
